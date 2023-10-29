1da52d85a60b9eae6624d89e2f01571dfc738369
devpdhana <devpdhansekar@gmail.com>
2023-10-29 13:34:40 +0530

    Final routing updated

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Box, Button, Typography} from '@mui/material'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
-  )
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Button LinkComponent={Link} to={"/books"} variant="contained">
+        <Typography variant="h3">Find All Books</Typography>
+      </Button>
+    </Box>
+  );
 }
 
 export default Home
